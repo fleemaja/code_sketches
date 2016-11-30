82c9151505106418a7cbb152590506ec0926237f
Drew Fleeman <drew.fleeman@gmail.com>
2016-11-30 17:26:56 -0600

    branch size shrinks recursively and add leaves

---
+++ README.md
@@ -0,0 +1,4 @@
+A place to store and version control my recent javascript projects hosted on Codepen. These projects will be things like games, data visualizations, and animations.
+
+### Links
+[Winter Spring Summer & Fall](http://codepen.io/fleemaja/pen/bBoBxK "Winter Spring Summer & Fall") - animated tree scene that changes with the seasons (the tree grows flowers and leaves in the spring, leaves change color in the fall, etc.)
--- winterSpringSummerFall/sketch.js
+++ winterSpringSummerFall/sketch.js
@@ -1,6 +1,13 @@
 var mountains = [];
-// Gradient Constants
+// branch size constants
+var rootSize = 42;
+var branchRatio = 0.67;
+// gradient constants
 var c1, c2;
+// tree variables
+var tree = [];
+var leaves = [];
+var count = 0;
 
 function setup() {
   var cHeight = window.innerHeight;
@@ -14,11 +21,32 @@ function setup() {
 
   setGradient(0, 0, width, height, c1, c2);
 
+  // hard coded two mountains
   mountains.push(new Mountain(height - 200, height - 400, [134,171,178]));
   mountains.push(new Mountain(height - 50, height - 300, [94,124,126]));
 
   for (var j = 0; j < mountains.length; j++) {
     mountains[j].show();
+  }
+
+  var a = createVector(width / 2, height);
+  var b = createVector(width / 2, height - 200);
+  var root = new Branch(a, b, rootSize);
+
+  tree[0] = root;
+
+  for (var b = 0; b < 10; b++) {
+    addBranches();
+  }
+
+  for (var i = 0; i < tree.length; i++) {
+    tree[i].show();
+  }
+
+  for (var i = 0; i < leaves.length; i++) {
+    fill(255, 0, 100, 100);
+    noStroke();
+    ellipse(leaves[i].x + random(-5, 5), leaves[i].y + random(-20, 20), 8, 8);
   }
 }
 
@@ -40,7 +68,7 @@ function Mountain(minY, maxY, phil) {
   this.show = function() {
     noStroke();
     fill(this.fill);
-    // We are going to draw a polygon out of the wave points
+    // draw a mountain polygon across the width of the screen with perlin noise determined y values
     beginShape();
     vertex(0, height);
     for (var x = 0; x < width + 7; x += 7) {
@@ -51,4 +79,55 @@ function Mountain(minY, maxY, phil) {
     vertex(width, height);
     endShape(CLOSE);
   }
+}
+
+function Branch(begin, end, size) {
+  this.begin = begin;
+  this.end = end;
+  this.finished = false;
+  this.size = size;
+
+  this.show = function() {
+    stroke(32);
+    strokeWeight(this.size);
+    line(this.begin.x, this.begin.y, this.end.x, this.end.y);
+  }
+
+  this.branchA = function() {
+    var dir = p5.Vector.sub(this.end, this.begin);
+    dir.rotate(PI / 6);
+    dir.mult(branchRatio);
+    var newEnd = p5.Vector.add(this.end, dir);
+    var b = new Branch(this.end, newEnd, this.size * branchRatio);
+    return b;
+  }
+  this.branchB = function() {
+    var dir = p5.Vector.sub(this.end, this.begin);
+    dir.rotate(-PI / 4);
+    dir.mult(branchRatio);
+    var newEnd = p5.Vector.add(this.end, dir);
+    var b = new Branch(this.end, newEnd, this.size * branchRatio);
+    return b;
+  }
+}
+
+function addBranches() {
+  for (var i = tree.length - 1; i >= 0; i--) {
+    if (!tree[i].finished) {
+      tree.push(tree[i].branchA());
+      tree.push(tree[i].branchB());
+    }
+    tree[i].finished = true;
+  }
+  count++;
+
+  if (count > 4) {
+    for (var i = 0; i < tree.length; i++) {
+      if (!tree[i].finished) {
+        var leaf = tree[i].end.copy();
+        leaves.push(leaf);
+      }
+    }
+  }
+
 }
