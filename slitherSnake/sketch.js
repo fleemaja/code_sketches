0e45a0b692cbb61f5364de19095899ba394a62ae
Drew Fleeman <drew.fleeman@gmail.com>
2016-12-07 21:54:06 -0600

    add setTimeout delay after snake dies

--- slitherSnake/sketch.js
+++ slitherSnake/sketch.js
@@ -4,35 +4,40 @@ var ghost;
 var s;
 var tailLength = 50;
 var snakeSize = 32;
+var gamePaused = false;
 
 function setup() {
   createCanvas(window.innerWidth,window.innerHeight);
-  s = new Snake(0, height/2);
+  s = new Snake(width/4, height/2);
   // snake will chase an invisible ghost to steer
   ghost = new Ghost();
   pickLocation();
 }
 
 function draw() {
-  background(51);
+  background(245, 248, 250);
+  noStroke();
+  fill(54);
   textSize(16);
   text("use the arrow keys to move the snake", width/3, 30);
   text("(make sure the game window is in focus to use arrow keys)", width/3, 60);
   text("tail length: " + tailLength, width/8, 30);
 
-  ghost.update();
-  ghost.show();
+  if (!gamePaused) {
+    ghost.update();
+    ghost.show();
 
-  var ghostVect = createVector(ghost.x, ghost.y);
+    var ghostVect = createVector(ghost.x, ghost.y);
 
-  if (s.eat(food)) {
-    pickLocation();
+    if (s.eat(food)) {
+      pickLocation();
+    }
+
+    // snake follows the ghost to steer
+    s.seek(ghostVect);
+    s.update();
+    s.death();
   }
-
-  // snake follows the ghost to steer
-  s.seek(ghostVect);
-  s.update();
-  s.death();
   s.display();
 
   fill(255, 0, 100);
@@ -75,6 +80,7 @@ function Snake(x,y) {
   this.maxspeed = 3;
   this.maxforce = 0.2;
   this.history = [];
+  this.eatingVal = 0;
 
   // Method to update location
   this.update = function() {
@@ -91,6 +97,10 @@ function Snake(x,y) {
     this.position.add(this.velocity);
     // Reset accelerationelertion to 0 each cycle
     this.acceleration.mult(0);
+
+    if (this.eatingVal > 0) {
+      this.eatingVal -= 0.5;
+    }
   };
 
   this.applyForce = function(force) {
@@ -115,14 +125,23 @@ function Snake(x,y) {
   };
 
   this.reset = function() {
-     tailLength = 50;
-     this.history = [];
-     this.position = createVector(width/2, height/2);
+    gamePaused = true;
+    var that = this;
+    this.history.forEach(function(seg) {
+      seg.add(random(-10, 10), random(-10, 10));
+    });
+    setTimeout(function() {
+      tailLength = 50;
+      that.history = [];
+      that.position = createVector(width/2, height/2);
+      gamePaused = false;
+    }, 3000);
   }
 
   this.death = function() {
-    var outOfBoundsX = this.position.x < 0 || this.position.x > width;
-    var outOfBoundsY = this.position.y < 0 || this.position.y > height;
+    var offset = this.r/2;
+    var outOfBoundsX = this.position.x < offset || this.position.x > width - offset;
+    var outOfBoundsY = this.position.y < offset || this.position.y > height - offset;
     if (outOfBoundsX || outOfBoundsY) {
       this.reset();
     } else {
@@ -130,7 +149,7 @@ function Snake(x,y) {
         var pos = this.history[i];
         var d = dist(this.position.x, this.position.y, pos.x, pos.y);
         if (d < 2) {
-            this.reset();
+          this.reset();
         }
       }
     }
@@ -140,6 +159,7 @@ function Snake(x,y) {
     var d = dist(this.position.x, this.position.y, pos.x, pos.y);
     if (d < foodDiameter) {
       tailLength += 20;
+      this.eatingVal = 20;
       return true;
     } else {
       return false;
@@ -161,13 +181,30 @@ function Snake(x,y) {
     push();
     translate(this.position.x,this.position.y);
     rotate(theta);
-    ellipse(0, 0, this.r, this.r);
+    fill(0,210,150);
+    ellipse(0, -this.eatingVal, this.r, this.r);
+    fill(255, 0, 0);
+    rect(-2, -2 * this.eatingVal, 4, 15);
+    stroke(0);
     fill(255);
-    ellipse(-this.r/5, 0, 12, 12);
-    ellipse(this.r/5, 0, 12, 12);
-    fill(98);
-    ellipse(-this.r/5, 0, 6, 6);
-    ellipse(this.r/5, 0, 6, 6);
+    triangle(-10, -20, -10, 0, 0, 0);
+    triangle(10, -20, 10, 0, 0, 0);
+    fill(0,150,150);
+    noStroke();
+    ellipse(0, 0, this.r, this.r);
+    if (!gamePaused) {
+      fill(255);
+      ellipse(-this.r/5, 0, 12, 12);
+      ellipse(this.r/5, 0, 12, 12);
+      fill(98);
+      ellipse(-this.r/5, 0, 6, 6);
+      ellipse(this.r/5, 0, 6, 6);
+    } else {
+      fill(255);
+      textSize(12);
+      text("X", -this.r/4 - 1, 0);
+      text("X", 1, 0);
+    }
     pop();
   };
 }
