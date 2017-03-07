8eca8e4885b1ac41d33e0481f39a21c53f577d30
Drew Fleeman <drew.fleeman@gmail.com>
2017-03-06 22:36:57 -0600

    add transition animation on form submit

--- madGifs/madGifs.js
+++ madGifs/madGifs.js
@@ -3,12 +3,13 @@ const submitBtn = document.querySelector('input[type="submit"]');
 const txt = "In 356 B.C., Phillip of Macedonia, the ruler of a province in northern Greece, became the father of a bouncing baby $$noun-1$$ named Alexander. Alexander's teacher was Aristotle, the famous $$noun-2$$. When he was twenty years old, his father was murdered by $$celebrity-1$$, after which he became $$noun-3$$ of all Macedonia. In 334, he invaded Persia and defeated $$celebrity-2$$ at the battle of $$a-place$$. Later, at Arbela, he won his most important victory, over Darius the Third. This made him $$noun-4$$ $$silly-word$$ over all Persians. Then he marched to India, and many of his $$plural-1$$ died. After that, Alexander began drinking too much $$type-of-liquid$$, and at the age of 33, he died of an infection in the $$part-of-the-body$$. His last words are reported to have been, 'There are no more $$plural-2$$ to conquer.'";
 
 const main = document.querySelector('.main');
+const storyDiv = document.querySelector('.story');
 const imgGrid = document.querySelector('.image-grid');
 
 function unique(arr) {
     var hash = {}, result = [];
     for ( var i = 0, l = arr.length; i < l; ++i ) {
-        if ( !hash.hasOwnProperty(arr[i]) ) { //it works with objects! in FF, at least
+        if ( !hash.hasOwnProperty(arr[i]) ) {
             hash[ arr[i] ] = true;
             result.push(arr[i]);
         }
@@ -49,7 +50,9 @@ function generateResults(e) {
   } while (m);
 
   filledTxt = filledTxt.replace(/\$/g, '');
-  main.innerHTML = filledTxt;
+  storyDiv.innerHTML = filledTxt;
+  storyDiv.classList.add('active');
+  main.classList.remove('active');
   getGifs(inputVals);
 }
 
@@ -60,12 +63,13 @@ const textInputs = document.querySelectorAll('input[type="text"]');
 const form = document.querySelector('form');
 
 form.addEventListener('keyup', checkForm);
+form.addEventListener('change', checkForm);
 
 function checkForm() {
   submitBtn.disabled = false;
-  textInputs.forEach((input) => {
-    if (input.value == "") {
-      submitBtn.disabled = true;
-    }
-  });
+  // textInputs.forEach((input) => {
+  //   if (input.value == "") {
+  //     submitBtn.disabled = true;
+  //   }
+  // });
 }
