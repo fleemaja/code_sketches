03fa0f7c41d3ad21abdd7853a484886588b88f7b
Drew Fleeman <drew.fleeman@gmail.com>
2017-03-14 15:24:41 -0500

    add map for d3 state animals project

---
+++ stateAnimals/index.js
@@ -0,0 +1,18 @@
+var svg = d3.select("svg");
+
+var path = d3.geoPath();
+
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+  if (error) throw error;
+
+  svg.append("g")
+      .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path);
+
+  svg.append("path")
+      .attr("class", "state-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+});
