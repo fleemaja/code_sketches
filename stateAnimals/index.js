f74ffb1a457f6b07af1fb99afceb676ee8f1daa8
Drew Fleeman <drew.fleeman@gmail.com>
2017-03-17 11:05:48 -0500

    add follow along link highlight effect to map project

--- stateAnimals/index.js
+++ stateAnimals/index.js
@@ -87,3 +87,26 @@ var STATES = {
   '55': 'Wisconsin',
   '56': 'Wyoming'
 };
+
+// follow along nav highlight
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+function selectCategory(e) {
+  e.preventDefault();
+}
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+triggers.forEach(a => a.addEventListener('click', selectCategory));
