059460e1bd83a1042e7930571d23ebaeff405fc8
Drew Fleeman <drew.fleeman@gmail.com>
2017-04-11 15:51:48 -0500

    add shadows and interpolated spacing and colors

--- hexagonTunnel/sketch.js
+++ hexagonTunnel/sketch.js
@@ -1,4 +1,7 @@
 var hexagons = [];
+var from;
+var to;
+var startSize = 4;
 
 function setup() {
   var iHeight = constrain(window.innerHeight, 500, 900);
@@ -6,10 +9,20 @@ function setup() {
 
   createCanvas(iWidth, iHeight);
 
-  var hexSize = 3.125;
-  while (hexSize < 500) {
-    hexagons.push(new Hexagon(hexSize));
-    hexSize *= 2;
+  drawingContext.shadowBlur = 50;
+  drawingContext.shadowColor = 'black';
+
+  from = color(255, 255, 255, 0.9 * 255);
+  to = color(179, 255, 200, 0.9 * 255);
+
+  var hexSize = startSize;
+  var hexNum = 0;
+  while (hexSize < width) {
+    hexagons.push(new Hexagon(hexSize, hexNum));
+    // hexSize += 50;
+    var mult = map(hexSize, startSize, width, 1.13, 1.5);
+    hexSize *= mult;
+    hexNum += 3;
   }
 }
 
@@ -17,20 +30,35 @@ function draw() {
   background(51);
   hexagons.forEach(function(h) {
     h.render();
+    h.update();
   });
 }
 
-function Hexagon(size) {
+function Hexagon(size, rotation) {
   this.size = size;
+  this.rotation = rotation;
+  this.color = lerpColor(from, to, 0);
+}
+
+Hexagon.prototype.update = function() {
+  // this.size *= 1.0;
+  var col = map(this.size, startSize, width/3, 0, 1);
+  this.color = lerpColor(from, to, col);
+  this.rotation += 0.005;
+
+  // if (this.size > width) {
+  //   this.size = 20;
+  //   this.rotation = 0;
+  // }
 }
 
 Hexagon.prototype.render = function() {
   noFill();
-  strokeWeight(this.size / 4);
-  stroke(179, 255, 200);
+  strokeWeight(this.size * 0.29);
+  stroke(this.color);
   push();
   translate(width * 0.5, height * 0.5);
-  rotate(frameCount / this.size);
+  rotate(this.rotation);
   polygon(0, 0, this.size, 6);
   pop();
 }
